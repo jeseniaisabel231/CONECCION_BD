5dfc036f492bae3ba526a71ba41e43259d453fe0
jeseniaisabel231 <130105827+jeseniaisabel231@users.noreply.github.com>
2024-01-09 09:14:31 -0500

    coneccion base de datos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dbURL="jdbc:mysql://localhost:3306/estudiante";
+        String dbuserName="root";
+        String dbpassword="";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection casita= DriverManager.getConnection(dbURL, dbuserName, dbpassword);
+            if(casita != null){
+                System.out.println("Conectado exitosamente a la base de datos");
+            }
+
+            Statement statement;
+            statement=casita.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery("SELECT * FROM grades");
+            int id;
+            String nombre;
+            while(resultSet.next()){
+                id=resultSet.getInt("id");
+                nombre=resultSet.getString("nombre");
+                System.out.println(id + " " + nombre);
+            }
+
+        }catch(Exception ex){
+            System.out.println(ex);
+        }
     }
 }
